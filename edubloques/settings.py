622c010f451b4a5d9ff25654b2309bcba1a9bb42
cristianald00@hotmail.com <cristianald00@hotmail.com>
2020-03-23 23:36:30 -0400

    .

--- edubloques/settings.py
+++ edubloques/settings.py
@@ -133,8 +133,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
