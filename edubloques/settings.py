c91e0a6f294cdc027cae2792550abae9a21dcad6
cristianald00@hotmail.com <cristianald00@hotmail.com>
2020-03-24 00:53:26 -0400

    .

--- edubloques/settings.py
+++ edubloques/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+ 
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
